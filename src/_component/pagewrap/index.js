9d235c4081006099e84775fba7c94ea13fb09539
ansonchaan <ansonchan9523@gmail.com>
2019-10-08 14:39:00 +0800

    add scroll function

--- src/_component/pagewrap/index.js
+++ src/_component/pagewrap/index.js
@@ -1,34 +1,53 @@
-import React,{ Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect, useRef } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import smoothScroll from './scroll';
 
-import Header from '../header';
-import Home from '../../page/home';
-import Projects from '../../page/projects';
-import ProjectSingle from '../../page/single/project';
-import PageNotFound from '../../page/404';
+import Header from "../header";
+import Home from "../../page/home";
+import Projects from "../../page/projects";
+import ProjectSingle from "../../page/single/project";
+import PageNotFound from "../../page/404";
 
 
-class PageWrap extends Component{
 
-    render(){
-        return(
-            <div className={`body_wrap ${this.props.lang}`}>
-                <Header {...this.props} />
-                <Switch>
-                    <Route exact path="/:lang/" render={props => <Home {...props} />} />
-                    <Route exact path="/:lang/projects/" render={props => <Projects {...props} />} />
-                    <Route path="/:lang/project/:title/:page?/" render={props => <ProjectSingle {...props} />} />
-                    <Route path="/:lang/page-not-found/" render={props => <PageNotFound {...props} />} />
-                    <Redirect from="*" to={"/" + this.props.lang + "/page-not-found/"}/>
-                </Switch>
-            </div>
-        )
-    }
+const PageWrap = (props) => {
+    const bodyWrap = useRef(null);
+
+    useEffect(()=>{
+        const smooth = new smoothScroll('#bodyWrap',(s, y, h)=>{
+            //onScroll(s, y, h);
+        });
+        smooth.on();
+        smooth.showScrollBar();
+    },[bodyWrap]);
+
+    return (
+      <div ref={bodyWrap} id="bodyWrap" className={`body_wrap ${props.lang}`}>
+        <Header {...props} />
+        <Switch>
+          <Route exact path="/:lang/" render={props => <Home {...props} />} />
+          <Route
+            exact
+            path="/:lang/projects/"
+            render={props => <Projects {...props} />}
+          />
+          <Route
+            path="/:lang/project/:title/:page?/"
+            render={props => <ProjectSingle {...props} />}
+          />
+          <Route
+            path="/:lang/page-not-found/"
+            render={props => <PageNotFound {...props} />}
+          />
+          <Redirect from="*" to={"/" + props.lang + "/page-not-found/"} />
+        </Switch>
+      </div>
+    );
 }
 
 const mapStateToProps = state => {
-    return { lang: state.lang }
-}
+  return { lang: state.lang };
+};
 
 export default connect(mapStateToProps)(PageWrap);
